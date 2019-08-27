ce170c213af8ce7353aa006d19fbe2997cb80bf2
ZWkang <907747874@qq.com>
2019-08-28 01:59:03 +0800

    ADDED: add bin commander

---
+++ bin/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+
+const linecitats = require("../lib/server");
+
+// 解析 Node 进程执行时的参数
+commander
+  .version("1.0.0")
+  // .usage("[options]")
+  // .arguments("<source>")
+  .option("-p, --port <n>", "server port")
+  .option("-o, --host <n>", "server host")
+  .option("-r, --rootpath <n>", "server rootpath")
+  .option("-i, --index <val>", "auto find index default: index.html")
+  .option("-c, --cwd <path>", "setup cwd path")
+  .option("-d, --dir <val>", "watch dir")
+  .option("-a, --autoclose <val>", "auto close server")
+  .option("--no-cache", "no cache")
+  .option("--no-compress", "no compress source")
+  .parse(process.argv);
+
+// console.log(commander);
+
+const Server = new linecitats(commander);
+
+Server.init();
